dca0bdd08aabf8b470ad1b23a7691e07483f863c
saitki <aleiretaxiu@gmail.com>
2025-03-15 21:02:08 -0500

    Obtener el email y crear usuarios

--- Models/user.php
+++ Models/user.php
@@ -21,7 +21,25 @@ class User {
         return false;
     }
 
-    
+    public function getEmail($email) {
+        $query = $this->conn->prepare('SELECT * FROM users WHERE email = :email');
+        $query->execute(['email' => $email]);
+        $usuario = $query->fetch(PDO::FETCH_ASSOC);
+          
+        if ($usuario) {
+            return true;
+        }
+        return false;
+    }
+    public function createUser($name, $email, $password) {
+        $query = $this->conn->prepare('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+        $query->execute([
+            'name' => $name,
+            'email' => $email,
+            'password' => $password
+        ]);
+    }
+
     public static function isAuthenticated() {
         return isset($_SESSION['user']);
     }
