b1df0bf2b20b28774512dac79ce67dd3b09816d1
saitki <aleiretaxiu@gmail.com>
2025-03-16 16:34:45 -0500

    Ajuste de posicion de texto para evitar desbordamiento

--- Views/modalDeleteTask.php
+++ Views/modalDeleteTask.php
@@ -1,3 +1,10 @@
+<style>.title-modal-delete {
+    word-wrap: break-word; /* Rompe las palabras largas */
+    white-space: normal; /* Permite que el texto salte de línea */
+    overflow-wrap: break-word; /* Asegura el ajuste de texto */
+    max-width: 100%; /* Evita que el label sea más ancho que su contenedor */
+    display: block; /* Asegura que ocupe todo el ancho disponible */
+}</style>
 <div class="modal fade" id="DeleteModal" tabindex="-1" aria-labelledby="DeleteModalDeleteLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -6,11 +13,11 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
+        <label for="" class="title-modal-delete"></label>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary">Eliinar</button>
+        <button type="button" class="btn btn-primary">Eliminar</button>
       </div>
     </div>
   </div>
