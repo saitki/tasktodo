66f295202ef5b047b8ac5a65e34e15557dfe3f59
saitki <aleiretaxiu@gmail.com>
2025-03-16 18:10:02 -0500

    Preparacion de las funciones para las tareas

--- Models/task.php
+++ Models/task.php
@@ -9,12 +9,25 @@ class task {
         $this->conn = $database->getConnection();
     }
 
-    public function getAll() {
-        $query = "SELECT * FROM task";
+    public function getAllByUser($id) {
+        $query = "SELECT * FROM task where id_user = :id_user";
         $stmt = $this->conn->prepare($query);
-        $stmt->execute();
+        $stmt->execute(['id_user' => $id]);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+    public function createTask(){
+
+    }
+    public function updateTask(){
+
+    }
+    public function updateStatusTask(){
+
+    }
+    public function deleteTask(){
+
+    }
+
 
    
 }
