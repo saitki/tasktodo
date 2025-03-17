c49ae5efa1cc05e257bc50362d1a2ccb73564ca8
saitki <aleiretaxiu@gmail.com>
2025-03-16 19:24:22 -0500

    Traduccion

--- Views/modalAddTask.php
+++ Views/modalAddTask.php
@@ -18,7 +18,7 @@
         </form>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
         <button type="button" class="btn btn-primary">Añadir</button>
       </div>
     </div>
