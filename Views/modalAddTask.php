e4c59eef62da37642c6074b6938da66ef64a75bf
saitki <aleiretaxiu@gmail.com>
2025-03-16 22:08:26 -0500

    Correcion de datos y anexo para el envio de datos para crear tarea

--- Views/modalAddTask.php
+++ Views/modalAddTask.php
@@ -6,21 +6,24 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form>
-          <div class="mb-3">
+      <form method="POST" action="<?=$_SERVER['PHP_SELF']?>">
+        <input type="hidden" name="action" value="createTask">
+
+        <div class="mb-3">
             <label for="recipient-name" class="col-form-label">Titulo</label>
-            <input type="text" class="form-control" id="recipient-name">
+            <input type="text" class="form-control" id="recipient-name" name="title">
           </div>
           <div class="mb-3">
             <label for="message-text" class="col-form-label">Descripcion</label>
-            <textarea class="form-control" id="message-text"></textarea>
+            <textarea class="form-control" id="message-text" name="description"></textarea>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+            <button type="submit" class="btn btn-primary">Crear Tarea</button>
           </div>
         </form>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-        <button type="button" class="btn btn-primary">Añadir</button>
-      </div>
+
     </div>
   </div>
 </div>
